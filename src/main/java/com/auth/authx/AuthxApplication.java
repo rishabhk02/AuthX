6996085eb6580b0ada7a54c54eedaaa2617e30f6
Rishabh Kumrawat <rishabhkumrawat40@gmail.com>
2025-12-30 09:48:16 +0530

    Initialized the repo

---
+++ src/main/java/com/auth/authx/AuthxApplication.java
@@ -0,0 +1,13 @@
+package com.auth.authx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthxApplication.class, args);
+	}
+
+}
